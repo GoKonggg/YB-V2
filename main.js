39a9514e7bdad8fb0cfc9e80fade1ed1f191b9e8
GoKonggg <wiselyjansonhalim90@gmail.com>
2025-09-02 09:57:53 +0700

    update

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-        
-        const plusButton = document.getElementById('plus-button');
+
+    const plusButton = document.getElementById('plus-button');
+    if (plusButton) {
         const plusIcon = plusButton.querySelector('svg');
         const emblemMenu = document.getElementById('emblem-menu');
         const menuOverlay = document.getElementById('menu-overlay');
 
-        if (plusButton && emblemMenu && menuOverlay) {
+        if (emblemMenu && menuOverlay) {
             plusButton.addEventListener('click', (event) => {
                 event.stopPropagation();
-                
                 const isHidden = emblemMenu.classList.toggle('hidden');
                 menuOverlay.classList.toggle('hidden');
-                
                 if (!isHidden) {
                     plusIcon.style.transform = 'rotate(45deg)';
                 } else {
@@ -25,4 +24,28 @@ document.addEventListener('DOMContentLoaded', () => {
                 plusIcon.style.transform = 'rotate(0deg)';
             });
         }
+    }
+
+    const currentPage = window.location.pathname;
+    const navLinks = document.querySelectorAll('nav a');
+
+    navLinks.forEach(link => {
+        const linkPath = new URL(link.href).pathname;
+        const linkTextSpan = link.querySelector('span');
+
+        if (linkPath === currentPage) {
+            link.classList.remove('text-gray-600');
+            link.classList.add('text-pink-500');
+            if (linkTextSpan) {
+                linkTextSpan.classList.add('font-bold');
+            }
+        } else {
+            link.classList.add('text-gray-600');
+            link.classList.remove('text-pink-500');
+            if (linkTextSpan) {
+                linkTextSpan.classList.remove('font-bold');
+            }
+        }
     });
+
+});
